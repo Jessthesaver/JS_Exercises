0c4a115aafae7394b99ddf4562d504cba44c90ce
Jessthesaver <70416069+Jessthesaver@users.noreply.github.com>
2022-11-24 16:11:59 -0600

    Add files via upload

--- Problem10.js
+++ Problem10.js
@@ -7,28 +7,13 @@ class Person{
         return this.name+' '+this.lastname;
     }
     async ajaxGet(link){
-        try{
-            let data = this.getName();
-            let url= new URL(link);
-            let header = new Headers();
-
-            url.searchParams.append('Name:',data);
-            header.append('Accept', 'application/json');
-
-            let request = new Request(url, {
-                method:'GET',
-                headers: header
-            });
-            let response = await fetch(request);
-            if(!response.ok){
-                throw { status: response.status, statusText: response.statusText};
-            }
-            let json = await response.json();
-            return json;
-        }
-        catch(err){
-            throw new Error(`Ups, you did it again, error with status ${err}`);
-        }
+        const response = fetch(link)
+        if (!response.ok) {
+            console.log(response.status, response.statusText);
+        };
+        const data = await response.json();
+        console.log(data)
+        return data;
     }
 }
 
