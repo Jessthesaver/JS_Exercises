84d9ff6ac49864522e2001bc17e416ba29b6289c
Jessthesaver <70416069+Jessthesaver@users.noreply.github.com>
2023-01-27 12:48:07 -0600

    Add files via upload

--- Problem39/main.js
+++ Problem39/main.js
@@ -2,14 +2,13 @@ function validator(input){
     let pattern= input.slice(0,3);
     for(let i =3; i< input.length; i++){
         pattern= pattern.slice(1)+input.charAt(i);
-        if(keyboardfloors[0].includes(pattern)||keyboardfloors[1].includes(pattern)||
-        keyboardfloors[2].includes(pattern)||keyboardfloors[3].includes(pattern)||
-        reversekeyboardfloors[0].includes(pattern)||reversekeyboardfloors[1].includes(pattern)||
-        reversekeyboardfloors[2].includes(pattern)||reversekeyboardfloors[3].includes(pattern)){
-            return false;
-        }
+        totalKeyboard.forEach(floor=>{
+            if(floor.includes(pattern)){
+                return true
+            }
+        })
     }
-    return true;
+    return false;
 }
 
 const keyboard= '1234567890qwertyuiopasdfghjklñzxcvbnm'
@@ -17,6 +16,7 @@ const reversekeyboard ='0987654321poiuytrewqñlkjhgfdsamnbvcxz'
 const result=document.querySelector('#results')
 const [...keyboardfloors]=[keyboard.slice(0,10),keyboard.slice(10,20),keyboard.slice(20,30),keyboard.slice(30)]
 const [...reversekeyboardfloors]=[reversekeyboard.slice(0,10),reversekeyboard.slice(10,20),reversekeyboard.slice(20,30),reversekeyboard.slice(30)]
+const totalKeyboard=keyboardfloors.concat(reversekeyboardfloors)
 const inp = document.querySelector('#txtinp');
 inp.addEventListener('input',(event)=>{
     result.textContent='';
