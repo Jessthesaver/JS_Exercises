2317fcf5fa38195937e96923ade22ecdb5777185
Jessthesaver <70416069+Jessthesaver@users.noreply.github.com>
2023-03-15 12:17:19 -0600

    Add files via upload

--- Problem17/index.js
+++ Problem17/index.js
@@ -1,15 +1,14 @@
 let candidate = {
-    name: {
-       firstname: 'John',
-       lastname: 'Galt',
-       phone: '123-456-7890'
-    },
-    printName: function(){
-        return this.name.firstname +' '+ this.name.lastname;
-    }
-}
+  name: {
+    firstname: "John",
+    lastname: "Galt",
+    phone: "123-456-7890",
+  },
+  printName: function () {
+    return `${this.name.firstname} ${this.name.lastname}`;
+  },
+};
 
-let div = document.querySelector('#body').querySelector('.info');
-let [div1,div2] = div.children
-div1.textContent = candidate.name.firstname;
-div2.textContent = candidate.printName();
+let div = document.querySelectorAll("#body .info div");
+div[0].textContent = candidate.name.firstname;
+div[1].textContent = candidate.printName();
