f02ac81e5f2a433cd6d2e8753e88ba091a72186a
Jessthesaver <70416069+Jessthesaver@users.noreply.github.com>
2023-03-15 12:20:54 -0600

    Add files via upload

--- Problem31/main.js
+++ Problem31/main.js
@@ -1,18 +1,33 @@
-const div= document.querySelector('div');
-const togglers= document.querySelectorAll('.toggler')
-let ps=[]
-togglers.forEach(element=>{
-    element.nextElementSibling.style.visibility='hidden'
-})
+const div = document.querySelector("div");
+const togglers = document.querySelectorAll("p");
 
-togglers.forEach(element=>{
-    element.addEventListener('click',(event)=>{
-        let target= event.target;
-        let sibbling = target.nextElementSibling;
-        if(sibbling.style.visibility==='visible'){
-            sibbling.style.visibility='hidden';
-        }else{
-            sibbling.style.visibility='visible';
-        }
-    });
+for (let i = 0; i < togglers.length; i++) {
+  if (i % 2 !== 0) {
+    togglers[i].style.visibility = "hidden";
+  }
+}
+
+div.addEventListener("click", (event) => {
+  if (event.target.textContent.includes("Toggle")) {
+    if (event.target.nextElementSibling.style.visibility === "visible") {
+      event.target.nextElementSibling.style.visibility = "hidden";
+    } else {
+      for (let index = 1; index < togglers.length; index = index + 2) {
+        togglers[index].style.visibility = "hidden";
+      }
+      event.target.nextElementSibling.style.visibility = "visible";
+    }
+  }
 });
+
+// togglers.forEach((element) => {
+//   element.addEventListener("click", (event) => {
+//     let target = event.target;
+//     let sibbling = target.nextElementSibling;
+//     if (sibbling.style.visibility === "visible") {
+//       sibbling.style.visibility = "hidden";
+//     } else {
+//       sibbling.style.visibility = "visible";
+//     }
+//   });
+// });
