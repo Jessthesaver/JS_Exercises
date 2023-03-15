0b960eb56520bd6d262f62b9fee456e7678ecd9b
Jessthesaver <70416069+Jessthesaver@users.noreply.github.com>
2023-03-15 12:19:25 -0600

    Delete Problem28.js

--- Problem28.js
+++
@@ -1,9 +0,0 @@
-let async = { getAll: function ( urlArray, callback){ /* … */ } };
-/* 
-Usage: async.getAll([axCall1,axCall2], callback);
-Expected context of callback:
-   {
-     1: <result of first function>,
-     2: <result of second function>
-   }
-*/
