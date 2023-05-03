fc1993550036a73488705fb259acfcb1cf49612e
Jessthesaver <70416069+Jessthesaver@users.noreply.github.com>
2023-05-03 12:09:38 -0600

    Add files via upload

--- Problem19.js
+++ Problem19.js
@@ -1,42 +1,45 @@
 const str = "potato";
-const pttr = "pot";
+const pttr = "a*";
 
 function check(str, pattern) {
   let success = [];
-  let failure = [];
+  let failure = false;
   let splitStr = [...str];
   let splitPattern = [...pattern];
 
   for (let letter = 0; letter < splitStr.length; letter++) {
     for (let element = 0; element < splitPattern.length; element++) {
-      if (splitPattern[element] === "*" && letter === element) {
-        success.push(splitPattern[element]);
-      } else {
-        if (splitPattern[element].charAt() === splitStr[letter].charAt()) {
-          success.push(splitStr[element]);
-        } else if (
-          splitPattern[element].charAt() !== splitStr[letter].charAt()
-        ) {
-          failure.push(splitStr[element]);
-        }
+      if (failure === true) {
+        break;
+      }
+      if (
+        (splitPattern[element] === "*" &&
+          splitStr[letter + element] &&
+          letter === element) ||
+        splitPattern[element] === splitStr[letter + element]
+      ) {
+        success.push(splitStr[letter + element]);
+      } else if (splitPattern[element].charAt() !== splitStr[letter].charAt()) {
+        failure = true;
       }
     }
   }
+
   success = success.join("");
 
   if (success.length === pattern.length) {
     return success;
-  }
-  if (success && failure.length === 0) {
-    return success;
-  } else if (failure.length !== 0 || failure.length === 0) {
-    return pattern;
+  } else if (failure) {
+    return false;
   }
 
   return { success, failure };
 }
 
 function test(stra, strb) {
+  if (strb.length > stra.length) {
+    return false;
+  }
   if (strb.includes("*")) {
     return check(stra, strb);
   } else {
